d631453142aeb150d691871474bee4e9df46281e
pranjalr004 <pranjalr376@gmail.com>
2025-02-18 22:41:18 +0530

    Dashboard Created

--- my-app/src/pages/Dashboard.tsx
+++ my-app/src/pages/Dashboard.tsx
@@ -1,9 +1,51 @@
+import { useNavigate } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
+import { EuiCard, EuiFlexGroup, EuiFlexItem, EuiIcon, EuiImage } from '@elastic/eui'
+import dashboard1 from "../assets/dashboard1.png"
+import dashboard2 from "../assets/dashboard2.png"
+import dashboard3 from "../assets/dashboard3.png"
 
 export default function Dashboard() {
   useAuth()
-  return <div>
-      Dashboard
+  const navigate=useNavigate()
+  return (
+    <>
+    <div style={{
+      display:'flex',
+      height:'100vh',
+      flexDirection:'column'
+    }}>
+      <EuiFlexGroup justifyContent='center' alignItems='center' style={{margin:'5vh 10vw'}}>
+        <EuiFlexItem>
+          <EuiCard
+          icon={<EuiImage size='5rem' alt='icon' src={dashboard1}/>}
+          title={`Create Meeting`}
+          description="Create a new meeting and invite people"
+          onClick={()=>navigate("/create")}
+          paddingSize='xl'
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiCard
+          icon={<EuiImage size='100%' alt='icon' src={dashboard2}/>}
+          title={`My Meetings`}
+          description="View your created Meetings"
+          onClick={()=>navigate("/mymeetings")}
+          paddingSize='xl'
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiCard
+          icon={<EuiImage size='5rem' alt='icon' src={dashboard3}/>}
+          title={`Meetings`}
+          description="View the meetings that are invited to"
+          onClick={()=>navigate("/create")}
+          paddingSize='xl'
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </div>
+    </>
+  )
   
 }
