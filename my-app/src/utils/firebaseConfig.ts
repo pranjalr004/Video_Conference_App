3694cb3320831a8a3376142f54900763677d4608
pranjalr004 <pranjalr376@gmail.com>
2025-02-18 10:08:20 +0530

    FirebaseDB Setup completed

---
+++ my-app/src/utils/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+import {collection,getFirestore} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCILSuVw9Y-t_8pZZAIwsL8_8K-HTW6uHY",
+  authDomain: "zoom-clone-59ea0.firebaseapp.com",
+  projectId: "zoom-clone-59ea0",
+  storageBucket: "zoom-clone-59ea0.firebasestorage.app",
+  messagingSenderId: "287027362936",
+  appId: "1:287027362936:web:ab09a42a57b28445e3418d",
+  measurementId: "G-16693SZC0W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth=getAuth(app)
+export const firebaseDB=getFirestore(app)
