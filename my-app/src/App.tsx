094347e132ca704ba5463a061e68eee8e5fd6904
pranjalr004 <pranjalr376@gmail.com>
2025-02-18 09:40:58 +0530

    Redux Toolkit Setup completed

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,18 @@
 import { EuiProvider } from '@elastic/eui'
 import React from 'react'
 import "@elastic/eui/dist/eui_theme_light.json"
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
  function App() {
     return (
   <EuiProvider>
-    <div>
-      App
-    </div>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/' element={<Dashboard/>}/>
+      <Route path='*' element={<Dashboard/>}/>
+    </Routes>
   </EuiProvider>
   )
 }
